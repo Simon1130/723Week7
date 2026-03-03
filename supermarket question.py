5edd4d125fabbde33a30ba65fb6474e1c309b1da
Simon1130 <3075302l@student.gla.ac.uk>
2026-03-03 17:30:26 +0000

    adding attritube and changing function name
    add some code comment

--- supermarket question.py
+++ supermarket question.py
@@ -7,13 +7,17 @@ Created on Tue Mar  3 17:10:16 2026
 """
 
 class Supermarket:
-    def __init__(self, frozen_foods, fresh_foods, vegatables):
+#each food item in the class should be dictionary, currency is integer
+#key pair is the name
+#value pair is the list of amount and price
+    def __init__(self, frozen_foods, fresh_foods, vegatables, currency):
         self.frozen_foods = frozen_foods
         self.fresh_foods = fresh_foods
         self.vegatables = vegatables
+        self.currency = currency
         
-    def buy_foods(self, foods, amount):
-        self.foods += amount
+    def buy_in_stock(self, foods, amount):
+        self.foods[1] += amount
         print(self.foods)
         
     def selling_foods(self, foods, amount):
